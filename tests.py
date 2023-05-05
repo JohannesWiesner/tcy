74b441dbb8ac85cd35ad75aafe951bc0049f0d57
JohannesWiesner <joh.wiesner@gmail.com>
2023-05-05 17:29:25 +0200

    improved tests. outsourced a function which only purpose is to return a list of affects cells (get_concerned_cells). Next step would be switch over to a class which contains all the tests that check the validity of the tsv file

--- tests.py
+++ tests.py
@@ -6,18 +6,33 @@ Tests for GitHub Actions
 
 import numpy as np
 import pandas as pd
+import string
+import pytest
 
-# FIXME: Would be more nice to get 'coordinates' as you see them in LibreOffice
-# or Excel (e.g. A1,B5, etc.)
-# FIXME: Not really necessary to do this for the whole .tsv file. Only for
-# the columns that are needed to generate the .yml file. See function below,
-# we could reduce dataframe to a subset of columns
+df = pd.read_csv('./packages.tsv',sep='\t',index_col=None,header=0)
+EXCEL_MAPPER = dict(zip(df.columns,string.ascii_uppercase))
+
+def get_concerned_cells(boolean_df):
+    '''Take in a boolean data frame where a True value indicates an error
+    for that cell. Return the column and index name of the concerned cells.
+    Use Excel-Style output (e.g. A1, Z3, etc.) which makes it easier for users
+    to find the concerned cells in their spreadsheet program'''
+    
+    boolean_df.columns = boolean_df.columns.map(EXCEL_MAPPER)
+    boolean_df.index = boolean_df.index + 2
+    cells = boolean_df.reset_index().melt(id_vars='index').query('value == True')
+    cells = cells['variable'] + cells['index'].astype(str)
+    cells = cells.tolist()
+    
+    return cells
+
 def test_check_whitespaces():
-    '''Ensure that no cell in the .tsv file has leading or trailing whitespaces'''
+    '''Ensure that cells in the .tsv file have neither leading or trailing
+    whitespaces'''
 
     df = pd.read_csv('./packages.tsv',sep='\t',index_col=None,header=0)
-    
-    # create a boolean mask that where True tells that a cell has whitespaces
+
+    # create a boolean mask 
     def create_mask(cell):
         if pd.isna(cell):
             return False
@@ -30,44 +45,38 @@ def test_check_whitespaces():
     
     # give user feedback which cells are concerned
     if whitespace_mask.any(axis=None):
-        cells_with_whitespaces = df.where(whitespace_mask).stack()
-        print('These cells have either leading or trailing whitespaces')
-        for idx,value in cells_with_whitespaces.items():
-            print(f"Index: {idx[0]}, Column: {idx[1]}, Value: {value}")
-    
-    # throw assertion error if the boolean mask has any true values
-    assert whitespace_mask.any(axis=None) == False
+        cells = get_concerned_cells(whitespace_mask)
+        message = f"These cells have either leading or trailing whitespaces: {cells}"
+        pytest.fail(message)
     
 def test_check_filled_out_columns():
     '''Check that certain columns are filled out'''
 
-    columns_to_check = ['package_name','language']
-    
     df = pd.read_csv('./packages.tsv',sep='\t',index_col=None,header=0)
+    columns_to_check = ['package_name','language']
     df = df[columns_to_check]
-    column_results = df.isna().any()
+    nan_mask = df.isna()
     
-    if any(column_results):
-        columns_with_nans = column_results[column_results == True]
-        print('These columns must not contain NaNs, i.e. have values in every cell:')
-        [print(col) for col in columns_with_nans.index.tolist()]
-    
-    assert any(column_results) == False
-    
+    if any(nan_mask):
+        cells = get_concerned_cells(nan_mask)
+        message = f"These cells must not contain NaNs, i.e. be filled out: {cells}"
+        pytest.fail(message)
+        
 def test_valid_options():
     '''Some columns in the .tsv file must only contain certain values'''
-    
 
     df = pd.read_csv('./packages.tsv',sep='\t',index_col=None,header=0)
-    
+
     # set up valid options 
-    valid_options_dict = {'package_manager':['conda','pip','cran'],
+    valid_options = {'package_manager':['conda','pip','cran'],
                      'language':['python','R','julia'],
-                     'bug_flag':['linux','windows','cross-platform',np.nan]}
-    
+                     'bug_flag':['linux','windows','cross-platform',np.nan]
+                     }
+
     # check each column and throw error if necessary
-    for column,valid_options in valid_options_dict.items():
-        column_results = df[column].unique()
-        column_check = set(column_results).issubset(valid_options)    
-        assert_info = f"The column {column} must only contain the following values: {valid_options} but contains {column_results}"
-        assert column_check == True, assert_info
+    for column,options in valid_options.items():
+        unique_column_values = df[column].unique()
+        column_check = set(unique_column_values).issubset(options)    
+        if column_check == False:
+            message = f"The column '{column}' must only contain the following values: {options} but contains {unique_column_values}"
+            pytest.fail(message)
