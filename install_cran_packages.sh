730aaa0d6fd1cf2ca3c3f9ac87ed9884eb061c5d
JohannesWiesner <joh.wiesner@gmail.com>
2023-05-19 14:17:37 +0200

    delete files to recheck output

--- install_cran_packages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-CONDA_BASE=$(conda info --base) && source $CONDA_BASE/etc/profile.d/conda.sh
-conda activate foo && Rscript -e "install.packages(c('PMA','lme4'),repos='https://cloud.r-project.org')"
