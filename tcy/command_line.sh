20dba3709a14fffb89aa471b4547c264f07c84af
JohannesWiesner <joh.wiesner@gmail.com>
2024-04-10 13:48:24 +0200

    improved script

--- tcy/command_line.sh
+++ tcy/command_line.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-# This script captures both positional and named arguments
-# and passes them on to tcy.
+# This script is used to provide tcy as a command-line application
+# after users install tcy via pip. It passes all command-line arguments
+# over to tcy.
 
-if [[ ! $@ ]]; then
+if (( $# == 0 )); then
     python3 -m tcy -h
 else
-    python3 -m tcy $@
+    python3 -m tcy "$@"
 fi
+
